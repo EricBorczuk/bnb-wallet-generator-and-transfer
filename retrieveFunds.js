93865549f292266798dd45ab9c6c0475e649dd47
Eric Borczuk <kuzcrob@gmail.com>
2025-07-07 12:45:04 -0400

    More changes, added .0002 threshold

--- retrieveFunds.js
+++ retrieveFunds.js
@@ -4,16 +4,17 @@ const Database = require('better-sqlite3')
 async function retrieveFunds(dbName, centralWalletAddress) {
   const results = []
   const db = new Database('bnb_wallets.db')
-  const walletRows = db.prepare('SELECT * FROM wallet WHERE balance > 0').all();
+  const walletRows = db.prepare('SELECT * FROM wallet WHERE balance > 0.0002').all();
   wallets = walletRows.map(walletRow => {
     return { "address": walletRow.addr, "privateKey": walletRow.pk }
   })
-  console.log(`${wallets.length} wallets with non-zero balance found in database`)
+  console.log(`${wallets.length} wallets with threshold balance found in database`)
   for (const wallet of wallets) {
     const balance = await web3.eth.getBalance(wallet.address)
+    let skip = false;
     if (new BN(balance).isZero()) {
       console.log(`No funds to retrieve from ${wallet.address}`)
-      throw new Error(`No funds to retrieve from ${wallet.address}`)
+      skip = true
     }
     console.log(`Balance of ${wallet.address} is ${web3.utils.fromWei(balance, "ether")} BNB`)
 
@@ -32,28 +33,33 @@ async function retrieveFunds(dbName, centralWalletAddress) {
 
     if (new BN(transaction.value).lte(new BN(0))) {
       console.log(`Insufficient funds to cover the fee from ${wallet.address}`)
-      throw new Error(`Insufficient funds to cover the fee from ${wallet.address}`)
+      skip = true
     }
 
     const amountTransferred = web3.utils.fromWei(transaction.value, "ether")
     const gasFee = web3.utils.fromWei(fee.toString(), "ether")
     let nonce = await web3.eth.getTransactionCount(wallet.address, "pending") // Get the current nonce
     // nonce++
-    const receipt = await performTransfer(wallet.address, wallet.privateKey, centralWalletAddress, amountTransferred, nonce)
-    if (!receipt.status) {
-      throw new Error(`Transaction failed at ${wallet.address}, transaction hash: ${receipt.transactionHash}`)
-    }
+    let status = "Success"
+    if (!skip) {
+      const receipt = await performTransfer(wallet.address, wallet.privateKey, centralWalletAddress, amountTransferred, nonce)
+      if (!receipt.status) {
+        console.log(`Transaction failed at ${wallet.address}, transaction hash: ${receipt.transactionHash}`)
+        status = "Failure"
+      }
 
-    results.push({
-      timestamp: receipt.timestamp,
-      from: wallet.address,
-      to: centralWalletAddress,
-      transactionHash: receipt.transactionHash,
-      status: "Success",
-      amountTransferred: amountTransferred,
-      gasFee: gasFee,
-    })
-    console.log(`Transferred ${amountTransferred} BNB from ${wallet.address} to ${centralWalletAddress}, TxHash: ${receipt.transactionHash}, gasFee: ${gasFee}`)
+      results.push({
+        timestamp: receipt && receipt.timestamp,
+        from: wallet.address,
+        to: centralWalletAddress,
+        transactionHash: receipt && receipt.transactionHash,
+        status: status,
+        amountTransferred: amountTransferred,
+        gasFee: gasFee,
+      })
+      if (status === "Success")
+        console.log(`Transferred ${amountTransferred} BNB from ${wallet.address} to ${centralWalletAddress}, TxHash: ${receipt.transactionHash}, gasFee: ${gasFee}`)
+    }
   }
   return results
 }
@@ -65,6 +71,8 @@ async function main() {
     process.exit(1)
   }
 
+  console.log(`Using database ${dbName} as source, out to ${centralWalletAddress}`)
+
   try {
     const results = await retrieveFunds(dbName, centralWalletAddress)
     let format = "csv"
@@ -73,9 +81,9 @@ async function main() {
       results.forEach(result => {
         content += `"${result.timestamp}","${result.from}","${result.to}","${result.transactionHash}","${result.status}","${result.amountTransferred}","${result.gasFee}"\n`
       })
-      writeDataToFile("retrieveFunds-transactions", content, "csv") // Ensure proper filename for CSV
+      // writeDataToFile("retrieveFunds-transactions", content, "csv") // Ensure proper filename for CSV
     } else {
-      writeDataToFile("retrieveFunds-transactions", results, "json") // Ensure proper filename for JSON
+      // writeDataToFile("retrieveFunds-transactions", results, "json") // Ensure proper filename for JSON
     }
   } catch (error) {
     console.error("An unexpected error occurred:", error.message)
--- updateBalances.js
+++ updateBalances.js
@@ -1,8 +1,17 @@
 const { web3, BN } = require("./utils")
 const Database = require('better-sqlite3')
+const syncRequest = require('sync-request')
 
 async function main() {
   try {
+    let bnbPrice = 0
+    try {
+      const res = syncRequest('GET', 'https://api.binance.us/api/v1/ticker/price?symbol=BNBUSD')
+      bnbPrice = parseFloat(JSON.parse(res.getBody('utf8')).price)
+    } catch (error) {
+      console.error('Request failed:', error.message);
+    }
+    console.log(`BNB price is ${bnbPrice}`)
     const db = new Database('bnb_wallets.db')
     const walletRows = db.prepare('SELECT * FROM wallet').all()
     const balances = []
@@ -11,13 +20,18 @@ async function main() {
       const balance = await web3.eth.getBalance(walletRow.addr)
       const etherBalance = web3.utils.fromWei(balance, 'ether')
       balances.push([etherBalance, nowDateStr, walletRow.addr])
-    }
+    }    
 
     const updateStmt = db.prepare('update wallet set balance = ?, balance_as_of = ? where addr = ?');
+    let total = 0
     balances.forEach(balanceItem => {
         console.log(`Updating to ${balanceItem[0]} for ${balanceItem[2]}`)
-        updateStmt.run(Number(balanceItem[0]), balanceItem[1], balanceItem[2])
+        const balanceNum = Number(balanceItem[0])
+        total += balanceNum
+        updateStmt.run(balanceNum, balanceItem[1], balanceItem[2])
     })
+    console.log(`Total BNB: ${total}`)
+    console.log(`Total USD: ${total * bnbPrice}`)
   } catch (error) {
     console.error("An unexpected error occurred:", error)
     process.exit(1)
